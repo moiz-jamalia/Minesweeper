2240a022800b0410cac93e11693e695e20e19469
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-04-28 13:43:03 +0200

    asdf

---
+++ src/Sweeper.java
@@ -0,0 +1,15 @@
+
+
+public class Sweeper {
+   private String moin;
+
+    public String getMoin() {
+        return moin;
+    }
+
+    public void setMoin(String moin) {
+        this.moin = moin;
+    }
+
+
+}
