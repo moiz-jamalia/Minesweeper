d55711a4c3482db68a13707aa40db85ee4ace075
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-04-28 14:27:22 +0200

    new datatype

--- src/Sweeper.java
+++ src/Sweeper.java
@@ -3,6 +3,12 @@
 public class Sweeper {
    private String moin;
    private String bla;
+
+    public Sweeper(String bla) {
+        this.bla = bla;
+    }
+
+
     public String getMoin() {
         return moin;
     }
