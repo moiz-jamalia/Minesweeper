5e9d8d54097d61a53e00a24b47d78800d86cfe3c
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-05-11 20:33:44 +0200

    neue Variablen

--- src/Sweeper.java
+++ src/Sweeper.java
@@ -1,21 +1,10 @@
 
 
 public class Sweeper {
-   private String moin;
-   private String bla;
 
-    public Sweeper(String bla) {
-        this.bla = bla;
-    }
+   private
 
 
-    public String getMoin() {
-        return moin;
-    }
-
-    public void setMoin(String moin) {
-        this.moin = moin;
-    }
 
 
 }
