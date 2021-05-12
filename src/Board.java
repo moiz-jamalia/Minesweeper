9f1028adf78407696330b001a29dbe690bc4f00c
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-05-12 16:11:25 +0200

    methoden difficulties: easy, medium und hard erstellt und fertig gemacht.

--- src/Board.java
+++ src/Board.java
@@ -1,8 +1,15 @@
+import java.util.Random;
+import java.util.Scanner;
+
 public class Board {
     private int[][] minen;
     private char[][] board;
     private int Line;
     private int column;
+    Random random = new Random();
+    Scanner input = new Scanner(System.in);
 
-
+    public Board(){
+        
+    }
 }
--- src/difficulties.java
+++ src/difficulties.java
@@ -1,13 +1,20 @@
 public class difficulties {
-    public static void easy(){
+    public static int[][] minen;
+    public static char[][] board;
 
+
+    public static void easy(){
+        minen = new int[10][10];
+        board = new char[8][8];
     }
 
     public static void medium(){
-
+        minen = new int[40][40];
+        board = new char[16][16];
     }
 
     public static void hard(){
-
+        minen = new int[100][100];
+        board = new char[30][16];
     }
 }
