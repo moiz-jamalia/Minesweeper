4486688834550d13b6df58545d1745d5a684c2ba
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-05-14 12:15:38 +0200

    correction

--- src/Game.java
+++ src/Game.java
@@ -14,8 +14,6 @@ public class Game {
     }
 
     public void test(){
-        board = new Board();
-        Play(board);
         System.out.println(Board.Xboard);
         System.out.println(Board.Yboard);
     }
--- src/Sweeper.java
+++ src/Sweeper.java
@@ -1,7 +1,7 @@
 public class Sweeper {
     public static void main(String[] args){
         Game game = new Game();
-       // game.Jogo();
+        game.Jogo();
         game.test();
     }
 }
