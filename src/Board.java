92e842fbcd554f723abd1c497bbc6d0f2a70f13f
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-05-14 21:12:22 +0200

    Variablen gesetzt

--- src/Board.java
+++ src/Board.java
@@ -115,7 +115,7 @@ public class Board {
     }
     public void show(){
         System.out.println("\n  lines");
-        for (int line = Xboard - 1; line > 0; line--){
+        for (int line = Xboard; line > 0; line--){
             System.out.print("\t" + line);
             for (int column = 0; column < Yboard; column++){
                 System.out.print("\t" + board[line][column]);
