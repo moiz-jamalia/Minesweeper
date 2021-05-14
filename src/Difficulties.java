df89fcdc7876c9c6322f044b7b7cb72561a916a6
moiz-jamalia <70012835+moiz-jamalia@users.noreply.github.com>
2021-05-14 13:47:00 +0200

    correction

--- src/Difficulties.java
+++ src/Difficulties.java
@@ -5,17 +5,17 @@ public class Difficulties {
 
     public static void easy(){
         minen = new int[10][10];
-        board = new char[8][8];
+        board = new char[9][9];
     }
 
     public static void medium(){
         minen = new int[40][40];
-        board = new char[16][16];
+        board = new char[17][17];
     }
 
     public static void hard(){
         minen = new int[100][100];
-        board = new char[30][16];
+        board = new char[31][17];
     }
 
     public static int[][] getMinen(){
